82a3d0835285fbbca3c03e7677afce1b59976df5
Vignesh Chekkichalil <vigneshchekkichalil@gmail.com>
2026-06-16 21:21:14 +0530

    Added P drive and removed W drive

---
+++ Close_P_drive.bat
@@ -0,0 +1,7 @@
+@echo off
+subst P: /D
+
+echo.
+echo P: drive removed successfully.
+echo.
+pause
--- EV_Battery_QA_Project/Close_W_drive.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-subst W: /D
-
-echo.
-echo W: drive removed successfully.
-echo.
-pause
